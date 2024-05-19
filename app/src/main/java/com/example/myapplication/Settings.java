846a55b5b116a97cc4c6f9673f6f1b50f176c449
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2024-05-19 19:11:55 -0400

    progress

--- app/src/main/java/com/example/myapplication/Settings.java
+++ app/src/main/java/com/example/myapplication/Settings.java
@@ -2,6 +2,13 @@ package com.example.myapplication;
 
 public class Settings {
 
+    String name;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
